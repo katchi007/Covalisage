255350bbb2c10331af19d2d8c9b4bb317dc8d9bd
katchi007 <oussema.essafi@gmail.com>
2018-05-05 12:52:06 +0100

    test

--- tests/AnnonceControllerTestBase.cs
+++ tests/AnnonceControllerTestBase.cs
@@ -9,21 +9,21 @@ namespace tests
 {
     public class AnnonceControllerTestBase
     {
-        private readonly HttpClient _client;
+     /*    private readonly HttpClient _client;
         public AnnonceControllerTestBase()
         {
             var server = new TestServer (new WebHostBuilder().UseStartup<Startup>());
             _client = server.CreateClient();
         }
 
-        [Fact]
+       [Fact]
         public void AnnonceGetAllTest()
         {
             var request = new HttpRequestMessage(new HttpMethod("GET"), "/api/annonce/all");
 
             var response = _client.SendAsync(request).Result;
             Assert.Equal(HttpStatusCode.OK , response.StatusCode);
-        }
+        }*/
         
     }
 }
